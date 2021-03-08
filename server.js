a3fd0f101d7b794d5bb8340e595502fb9f055e6c
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-08 23:09:38 +0400

    Adding new user properly set up with the mongoDb

--- server.js
+++ server.js
@@ -1,19 +1,60 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors({optionsSuccessStatus:200}));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use("/public",express.static(__dirname + "/public"));
+const port = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/views/index.html");
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('db connected');
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log("App is running on port: "+port);
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(__dirname + "/public"));
+
+const userSchema = new mongoose.Schema({
+    username: String
+});
+
+
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+})
+
+app.post("/api/exercise/new-user", (req, res) => {
+    var name = req.body.username;
+    User.find({ username: name }, (error, foundUser) => {
+        if (error) {
+            console.log(error);
+        } else {
+            if (foundUser.length == 0) {
+                const newUser = new User({ username: name });
+                newUser.save((error, success) => {
+                    if (error) {
+                        return console.log(error);
+                    } else {
+                        res.json({ "username": success.username, "_id": success._id });
+                    }
+                });
+            }else{
+                res.send("Username already taken");
+
+            }
+        }
+    });
+})
+
+app.listen(port, () => {
+    console.log("App is running on port: " + port);
 })
