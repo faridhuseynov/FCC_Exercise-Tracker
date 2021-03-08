9f6e5eec53092514b2741378982e0b09309fdccf
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-08 21:39:46 +0400

    dotenv package added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose= require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT||3000;
 
 app.use(cors({optionsSuccessStatus:200}));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,6 +14,6 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
 })
 
-app.listen(port,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("App is running on port: "+port);
 })
