f20f4ab840542e36d07ef12be4b4eb8227b09c44
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-08 19:48:07 +0400

    packages added, port connected

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose= require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT||3000;
+
+app.use(cors({optionsSuccessStatus:200}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use("public",express.static(__dirname+"/public"));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/views/index.html");
+})
+
+app.listen(port,()=>{
+    console.log("App is running on port: "+port);
+})
